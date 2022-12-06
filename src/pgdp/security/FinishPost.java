d2b4693b9156c3f238203f6fea43260eee4634af
cyanfhine <yifan03.chen@tum.de>
2022-12-06 17:52:54 +0100

    finishpost implementiert

--- src/pgdp/security/FinishPost.java
+++ src/pgdp/security/FinishPost.java
@@ -1,18 +1,36 @@
 package pgdp.security;
 
+import java.util.Arrays;
+
 public class FinishPost extends FlagPost {
     public FinishPost(int postNumber) {
         super(postNumber);
     }
 
     public boolean up(String type) {
-        super.up(type);
-        return false;
+        if (type == "end") {
+            setDepiction("chequered");
+            setLevel(5);
+            return true;
+        } else {
+            return up(type);
+        }
     }
 
     @Override
     public boolean down(String type) {
-        super.down(type);
-        return false;
+        return down(type);
+    }
+
+    public String toString() {
+        String output;
+        if (getLevel() == 0) {
+            output = "Signal post " + getPostNumber() + " of type finish post is in level " + getLevel()
+                    + " and is  doing nothing";
+        } else {
+            output = "Signal post " + getPostNumber() + " of type finish post is in level " + getLevel()
+                    + " and is  waving  " + getDepiction();
+        }
+        return output;
     }
 }
